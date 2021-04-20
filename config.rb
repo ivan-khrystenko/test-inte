3944085c1a316e1d93bdf2e9173b7b2d58b43ee8
Ivan <hristenko.i@gmail.com>
2021-04-20 11:10:44 +0300

    First Commit

---
+++ config.rb
@@ -0,0 +1,8 @@
+require 'compass/import-once/activate'
+
+# Configuration de la compilation
+http_path = "assets/"
+css_dir = "assets/css"
+sass_dir = "assets/sass"
+images_dir = "assets/img"
+javascripts_dir = "assets/js"
