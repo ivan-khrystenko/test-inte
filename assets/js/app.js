a07eda76a7d3d8a9e8899fc639056c9c9de498c4
Ivan <hristenko.i@gmail.com>
2021-04-20 11:21:58 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,8 @@
 /* Votre code JS ici */
+let parents = document.querySelectorAll('.article');
+for (let i = 0, parent; parent = parents[i]; i++)
+   parent.onclick = function (e) {
+      if (e.target.className == 'btn') {
+         this.classList.add('hide');
+      }
+   };
